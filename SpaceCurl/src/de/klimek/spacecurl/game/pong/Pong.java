3bb0d0537252b159beb54d3b7ab11b64f9463c78
Mike <mike@criduchat.de>
2014-03-04 17:47:29 +0100

    more fluid status for pong

--- SpaceCurl/src/de/klimek/spacecurl/game/pong/Pong.java
+++ SpaceCurl/src/de/klimek/spacecurl/game/pong/Pong.java
@@ -45,8 +45,9 @@ public class Pong extends GameFragment {
     private int mBallContacts;
     private boolean mFinished = false;
     private int mHighscore = 0;
-    private boolean mGameOver;
-    public float mStatus;
+    private boolean mStatusUpdate = true;
+    public float mStatus = 1.0f;
+    public float mStatusStepSize = 0.1f;
 
     private View mGame;
     private PongSettings mSettings;
@@ -58,9 +59,8 @@ public class Pong extends GameFragment {
         mBall = new Ball(getResources());
         Resources res = getResources();
         mShowLives = mSettings.showLives();
-        ;
         mTotalLives = mSettings.getLives();
-        ;
+
         mLives = new Lives(mTotalLives, res);
         mPaddleLeft = new Paddle(Paddle.PaddleSide.Left, res);
         mPaddleTop = new Paddle(Paddle.PaddleSide.Top, res);
@@ -71,7 +71,6 @@ public class Pong extends GameFragment {
         mPongLayout = (FrameLayout) rootView.findViewById(R.id.game_pong_layout);
         mPongScore = (TextView) mPongLayout.findViewById(R.id.game_pong_score);
         mGame = new View(getActivity()) { // Called back to draw the view. Also
-                                          // called by invalidate().
             @Override
             protected void onDraw(Canvas canvas) {
                 if (mShowLives) {
@@ -182,6 +181,7 @@ public class Pong extends GameFragment {
                 } else if (mLives.getLives() <= 0) {
                     mFinished = true;
                 } else {
+                    // reset
                     mBallContacts = 0;
                     mBall.setFaceState(Ball.FaceState.Normal);
                     mBall.setPositionX(mViewWidthMax / 2);
@@ -193,62 +193,85 @@ public class Pong extends GameFragment {
                     // mBall.mSpeed += mBallSpeedModifier;
                     // mBall.randomAngle();
                 }
+                return; // returns instead of "else if" for readability
             }
+
             // Check paddle collisions and react
-            else if (mBall.collidesWithPaddle(mPaddleTop)) {
+            // Top
+            if (mBall.collidesWithPaddle(mPaddleTop)) {
                 mBall.setSpeedY(Math.abs(mBall.getSpeedY()));
-                ++mBallContacts;
-                mHighscore = mBallContacts > mHighscore ? mBallContacts : mHighscore;
-                mBall.setRemainingFaceTime(500);
-                mBall.setFaceState(Ball.FaceState.Smiling);
-            } else if (mBall.collidesWithPaddle(mPaddleBottom)) {
+                doAfterCollision();
+                return;
+            }
+            // Bottom
+            if (mBall.collidesWithPaddle(mPaddleBottom)) {
                 mBall.setSpeedY(-Math.abs(mBall.getSpeedY()));
-                ++mBallContacts;
-                mHighscore = mBallContacts > mHighscore ? mBallContacts : mHighscore;
-                mBall.setRemainingFaceTime(500);
-                mBall.setFaceState(Ball.FaceState.Smiling);
-            } else if (mBall.collidesWithPaddle(mPaddleLeft)) {
+                doAfterCollision();
+                return;
+            }
+            // Left
+            if (mBall.collidesWithPaddle(mPaddleLeft)) {
                 mBall.setSpeedX(Math.abs(mBall.getSpeedX()));
-                ++mBallContacts;
-                mHighscore = mBallContacts > mHighscore ? mBallContacts : mHighscore;
-                mBall.setRemainingFaceTime(500);
-                mBall.setFaceState(Ball.FaceState.Smiling);
-            } else if (mBall.collidesWithPaddle(mPaddleRight)) {
+                doAfterCollision();
+                return;
+            }
+            // Right
+            if (mBall.collidesWithPaddle(mPaddleRight)) {
                 mBall.setSpeedX(-Math.abs(mBall.getSpeedX()));
-                ++mBallContacts;
-                mHighscore = mBallContacts > mHighscore ? mBallContacts : mHighscore;
-                mBall.setRemainingFaceTime(500);
-                mBall.setFaceState(Ball.FaceState.Smiling);
-            } else if (mBall.collidesWithWall(mViewWidthMin, mViewHeightMin, mViewWidthMax,
+                doAfterCollision();
+                return;
+            }
+
+            // Wall collision
+            if (mBall.collidesWithWall(mViewWidthMin, mViewHeightMin, mViewWidthMax,
                     mViewHeightMax)) {
                 mBall.setRemainingFaceTime(500);
                 mBall.setFaceState(Ball.FaceState.Frowning);
+
+                // update status
+                if (mBallContacts <= 0) { // bad
+                    mStatus -= 0.5f;
+                } else if (mBallContacts < 5) { // okay
+                    // 1 -> .4
+                    // 2 -> .3
+                    // 3 -> .2
+                    // 4 -> .1
+                    mStatus -= 0.5 - (mBallContacts / 10f);
+                }
+                mStatusUpdate = true;
+
+                // lost a life
                 if (mLives.getLives() > 0) {
                     mLives.setLives(mLives.getLives() - 1);
-                    mGameOver = true;
                 }
-            } else {
-                mBall.setRemainingFaceTime(mBall.getRemainingFaceTime() - deltaTime);
-                if (mBall.getRemainingFaceTime() <= 0) {
-                    mBall.setFaceState(Ball.FaceState.Normal);
-                }
+                return;
             }
+
+            // no collision and
+            mBall.setRemainingFaceTime(mBall.getRemainingFaceTime() - deltaTime);
+            if (mBall.getRemainingFaceTime() <= 0) {
+                mBall.setFaceState(Ball.FaceState.Normal);
+            }
+        }
+
+        private void doAfterCollision() {
+            ++mBallContacts;
+            mHighscore = mBallContacts > mHighscore ? mBallContacts : mHighscore;
+            mStatus += mStatusStepSize;
+            mStatus = Math.min(mStatus, 1.0f);
+            mStatusUpdate = true;
+            mBall.setRemainingFaceTime(500);
+            mBall.setFaceState(Ball.FaceState.Smiling);
         }
 
         @Override
         protected void onProgressUpdate(Void... values) {
             mPongScore.setText(Integer.toString(mBallContacts));
-            if (mGameOver) {
-                if (mBallContacts <= 0) { // bad
-                    mStatus = 0.0f;
-                } else if (mBallContacts >= 5) { // good
-                    mStatus = 1.0f;
-                } else {
-                    mStatus = (mBallContacts / 10.0f) * 2.0f; // okay
-                }
+            if (mStatusUpdate) {
                 notifyStatusChanged(mStatus);
-                mGameOver = false;
+                mStatusUpdate = false;
             }
+
             if (mFinished) {
                 boolean handled = notifyFinished(String.format("Beste Score: %d", mHighscore));
                 if (!handled) {
